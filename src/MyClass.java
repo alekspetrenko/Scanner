74fac48a5f467b0b24434c4a1d7e25b7bc2a48fb
Aleksandr Petrenko <petrenko.aleksandr025@gmail.com>
2016-08-24 02:17:05 +0300

    coutniswrong

--- src/MyClass.java
+++ src/MyClass.java
@@ -5,7 +5,7 @@ public class MyClass {
 	static String s;
 	private static Scanner scan;
 	private static String user_input_String;
-	private static String str;
+	
     int storage(String s) {
     return s.length();
 }
@@ -16,9 +16,9 @@ public class MyClass {
 	scan = new Scanner (System.in);
 	System.out.println("Enter some String");
 	user_input_String = scan.nextLine();
-	String str1 = new String();
-	str = "Строка из букв, цифр 492 и специальных символов %*;№?";
-	int length = str1.length();
+	
+	user_input_String = "Строка из букв, цифр  и специальных символов %*;№?";
+	int length = user_input_String.length();
 	System.out.println("Длина строки = " + length);
 	
 	

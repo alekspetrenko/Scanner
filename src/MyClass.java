a5c55f0819910447b35a98223bbc4a2752003147
Aleksandr Petrenko <petrenko.aleksandr025@gmail.com>
2016-08-24 02:10:21 +0300

    count0

--- src/MyClass.java
+++ src/MyClass.java
@@ -5,23 +5,25 @@ public class MyClass {
 	static String s;
 	private static Scanner scan;
 	private static String user_input_String;
+	private static String str;
     int storage(String s) {
     return s.length();
 }
 
 	
 	public static void main(String[]args) {
-	
-	
+		
 	scan = new Scanner (System.in);
 	System.out.println("Enter some String");
 	user_input_String = scan.nextLine();
+	String str1 = new String();
+	str = "Строка из букв, цифр 492 и специальных символов %*;№?";
+	int length = str1.length();
+	System.out.println("Длина строки = " + length);
 	
-	System.out.println("The value of the String is");
-	for (int i = 0; i < args.length; i++)
-		  
-        System.out.println(args[i]);
-	int output_String = s.length();
+	
+	  
+
 
 	
 	}
